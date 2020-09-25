173dbe14f662fe8134a55b5dd635297851bab599
Oliver Mason <omason@protonmail.com>
2020-09-25 20:25:10 +1200

    Header link and styling change

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,6 +2,7 @@
     <head>
         <title>Blog header</title>
         <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.css" >
+        <link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css" >
     </head>
 
     <body>
@@ -9,11 +10,12 @@
     <nav class="navbar navbar-expand-lg navbar-dark bg-primary">        
         <div class="container">
             <div class="navbar-header">
-                <a class="navbar-brand" href="/codeigniter"> CodeIgniter Blog</a>
+                <h1><a class="navbar-brand" href="/codeigniter"> CodeIgniter Blog</a></h1>
                 <div id="navbar">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item"> <a class="nav-link" href="<?php echo base_url(); ?>"> Home</a></li>
                         <li class="nav-item"> <a class="nav-link" href="<?php echo base_url(); ?>index.php/about">About</a></li>
+                        <li class="nav-item"> <a class="nav-link" href="<?php echo base_url(); ?>index.php/posts">Posts</a></li>
                     </ul>
                 </div>   
             </div>
