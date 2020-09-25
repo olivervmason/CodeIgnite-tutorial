de9154e66a3b6c4882b74b8115b7b012e34df381
Oliver Mason <omason@protonmail.com>
2020-09-25 20:24:47 +1200

    Routes for posts controller

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,8 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+$route['posts/(:any)'] = 'posts/view/index/$1';
+$route['posts'] = 'posts/index';
 $route['default_controller'] = 'pages/view';
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
