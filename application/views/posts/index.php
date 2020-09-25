e141049bcb4e769559dd53a615cde8f79806771f
Oliver Mason <omason@protonmail.com>
2020-09-25 23:45:59 +1200

    Fix header link

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -5,7 +5,7 @@
     </br>
     <h4> <?php echo $post['title']; ?> </h4>
     <p> <?php echo $post['body']; ?> </p>
-    <a class="btn btn-primary" href="<?php echo base_url('/posts/'.$post['slug']); ?>">See full post</a>
+    <a class="btn btn-primary" href="<?php echo base_url('index.php/posts/view/'.$post['slug']); ?>">See full post</a>
     <small class="post-date"> <?php echo "Posted on ", $post['created_at']; ?> </small>
     </br>
 <?php endforeach; ?>
