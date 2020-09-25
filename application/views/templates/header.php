be9e7d5e4999e4efb45d039dc1780660e3fbb2f4
Oliver Mason <omason@protonmail.com>
2020-09-25 16:31:13 +1200

    Add bootstrap link and header styling

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,6 +1,22 @@
 <html>
     <head>
         <title>Blog header</title>
+        <link rel="stylesheet" href="https://bootswatch.com/4/pulse/bootstrap.css" >
     </head>
 
     <body>
+
+    <nav class="navbar navbar-inverse">        
+        <div class="container">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/codeigniter"> CodeIgniter Blog</a>
+                <div id="navbar">
+                    <ul class="navbar-nav">
+                        <li> <a href="/codeigniter">Home</a></li>
+                        <li> <a href="index.php/about">About</a></li>
+                    </ul>
+                </div>   
+            </div>
+        </div>
+    </nav>
+
