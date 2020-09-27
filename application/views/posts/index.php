f78bb637dd766c34d719126981681f66fc267433
Oliver Mason <omason@protonmail.com>
2020-09-27 16:22:24 +1300

    Truncate post body on summary page

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -4,7 +4,7 @@
 <?php foreach($posts as $post) : ?>
     </br>
     <h4> <?php echo $post['title']; ?> </h4>
-    <p> <?php echo $post['body']; ?> </p>
+    <p> <?php echo word_limiter($post['body'], 82); ?> </p>
     <a class="btn btn-primary" href="<?php echo base_url('posts/view/'.$post['slug']); ?>">See full post</a>
     <small class="post-date"> <?php echo "Posted on ", $post['created_at']; ?> </small>
     </br>
