eedea96bcb744d55a526b9e3ade22d335ebed792
Oliver Mason <omason@protonmail.com>
2020-09-27 23:01:11 +1300

    Add drop down menu for post categories - has bug

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -32,6 +32,8 @@
         public function create(){
             $data['title'] = "Create Post";
 
+            $data['categories'] = $this->post_model->get_categories();
+
             // Validation from CodeIgnite libraries enabled in config->autoload.php;
             $this->form_validation->set_rules('title', 'Title', 'required');
             $this->form_validation->set_rules('body', 'Body', 'required');
